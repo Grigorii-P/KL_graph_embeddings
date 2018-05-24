ad6d8760543266a95cc7f0397bda5d4e00b25cb5
Grigorii-P <pogoreloff.g@gmail.com>
2018-05-24 13:34:48 +0300

    pca_mat with top 100 features + corrections

--- draft.py
+++ draft.py
@@ -1,6 +1,8 @@
 import pickle
 
-with open ('targets', 'rb') as fp:
-    itemlist = pickle.load(fp)
+l = [1,5,3]
+l = {'a':1,'b':5,'c':3}
+sort_vocab = sorted(l.items(),key=lambda x: x[1], reverse=True)
+sort_vocab = [x[0] for x in sort_vocab]
 
-print()
+print(sort_vocab)
--- pca_matrix_creation.py
+++ pca_matrix_creation.py
@@ -3,25 +3,22 @@ import os
 from time import time
 import json
 import pickle
-from random import shuffle
 
 path_chunks = '/home/pogorelov/data/chunks/data/'
 path_to_output = '/home/pogorelov/output.txt'
+path_to_dict = '/home/pogorelov/dict.txt'
 path_to_all_labels = '/home/pogorelov/work/KL_graph_embeddings/all_labels.txt'
-path_to_save_dict = '/home/pogorelov/dict.txt'
+path_to_pca_mat = 'pca_mat'
+path_to_targets = 'targets'
 
 # path_chunks = 'C:\Users\pogorelov_g\Desktop\work\chunks\\'
-# path_to_all_labels = 'all_labels.txt'
-# path_to_save_dict = 'dict.txt'
 # path_to_output = 'output.txt'
+# path_to_dict = 'dict.txt'
+# path_to_all_labels = 'all_labels.txt'
 
 def dict_of_unique_labels(graph_dict, count_subgraphs={}):
     for key in graph_dict:
         if type(graph_dict[key]) is dict:
-            if key not in count_subgraphs:
-                count_subgraphs[key] = 1
-            else:
-                count_subgraphs[key] += 1
             dict_of_unique_labels(graph_dict[key], count_subgraphs)
         else:
             if graph_dict[key] not in count_subgraphs:
@@ -37,10 +34,14 @@ def word_and_label(item, all_labels):
     return word, label
 
 def add_to_matrix(pca_matrix, count_subgraphs, sorted_vocab_list):
-    l = [0 for i in xrange(len(sorted_vocab_list))]
+    l = [0 for i in xrange(len(sorted_vocab_list)+1)]
     for key in count_subgraphs:
-        ind = sorted_vocab_list.index(key)
-        l[ind] = count_subgraphs[key]
+        try:
+            ind = sorted_vocab_list.index(key)
+            l[ind] = count_subgraphs[key]
+        except ValueError:
+            l[-1] += count_subgraphs[key]
+
     pca_matrix.append(l)
 
 
@@ -62,15 +63,23 @@ f = open(path_to_output, "w")
 start = time()
 with open(path_to_all_labels, 'r') as file:
     all_labels = json.loads(file.read())
+with open(path_to_dict, 'rb') as file:
+    dict_800k = pickle.load(file, encoding='latin1')
 
-dict_ = {}
+top_features = 100
+sort_vocab = sorted(dict_800k.items(), key=lambda x: x[1], reverse=True)
+sort_vocab = [x[0] for x in sort_vocab]
+sort_vocab = sort_vocab[:top_features]
+
 for file_ in files:
     try:
         chunk = joblib.load(path_chunks + file_)
     except:
         continue
     for i, item in enumerate(chunk):
-        dict_of_unique_labels(item[1], count_subgraphs=dict_)
+        word, target = word_and_label(item, all_labels)
+        add_to_matrix(pca_mat, word, sort_vocab)
+        targets.append(target)
     count_jbl += 1
     try:
         if count_jbl % num_chunks_to_print == 0:
@@ -80,6 +89,8 @@ for file_ in files:
 
 end = time()
 print >> f, 'created dict from %d chunks in %0.2f min' % (len(files), (end - start) / 60)
-with open(path_to_save_dict, 'wb') as fp:
-    pickle.dump(dict_, fp)
+with open(path_to_pca_mat, 'wb') as fp:
+    pickle.dump(pca_mat, fp)
+with open(path_to_targets, 'wb') as fp:
+    pickle.dump(targets, fp)
 f.close()
